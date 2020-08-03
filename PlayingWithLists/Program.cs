36a2c01b0110ffc4801900fdb26c317746b7bd55
Calvin Verheijen <ypoora@gmail.com>
2020-08-03 22:00:22 +0200

    Add project files.

---
+++ PlayingWithLists/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Dynamic;
+
+namespace PlayingWithLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Boxes = new List<Box>(); //Make a list. Lists are good places to stash boxes. I swear.
+
+            while(true) { //Make boxes in any colour you like, and put something in each one.
+                Console.WriteLine("What's the colour of your box?");
+                var colour = Console.ReadLine();
+
+                Console.WriteLine("What's inside?");
+                var contents = Console.ReadLine();
+
+                var Box = new Box(contents, colour);
+                Boxes.Add(Box);
+
+                Console.WriteLine($"\nCool, now you have a {Box.Colour} box containing {Box.Item}. Do you want another box to put something in? Y/N");
+                var another = Console.ReadKey(true);
+                if (another.KeyChar.ToString().ToLower() == "n") { break; }
+            }
+            Console.WriteLine("You have the following packed away in your stash:\n"); //Show off your stash of boxes and their contents. You're so organized.
+            foreach(var Box in Boxes)
+            {
+                Box.Describe();
+            }
+            Console.WriteLine("\nYour boxes are full. We should make some room. These items were not important, were they?"); //Too bad i'm a dick, i don't care about your items at all!!!
+            Console.WriteLine("Now we have:\n");
+            foreach (var Box in Boxes)
+            {
+                Box.Empty();
+                Box.Describe();
+            }
+            Console.WriteLine("Now tell me something you really really like a lot.");
+            var NewThing = Console.ReadLine();
+            Console.WriteLine("\nCool. Did ya a favour; here's your new stash!\n"); //I'm going to put your favourite thing into all of your boxes. Aren't i nice?
+            foreach(var Box in Boxes)
+            {
+                Box.Fill(NewThing);
+                Box.Describe();
+            }
+            
+
+        }
+    }
+    class Box
+    {
+        public string Item { get; private set; }  //Contents of box
+        public string Colour { get; private set; } //Colour of box
+
+        public Box(string item, string colour)  //Constructor: Give box a colour and some contents
+        {
+            Item = item;
+            Colour = colour;
+        }
+
+        public void Describe() //Tell me what is in my box!
+        {
+            Console.WriteLine($"A {Colour} box with {Item} inside.");
+        }
+
+        public void Empty() //Take everything out of my box!
+        {
+            Item = "nothing";
+        }
+
+        public void Fill(string item) //Put something into my box!
+        {
+            Item = item;
+        }
+    }
+}
---
+++ QuizApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Idiosyncracies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define score, set it to 0 and ask for participant name
+            var score = 0;
+            Console.WriteLine("Hi, what's your name?\n");
+            var name = Console.ReadLine();
+            Console.WriteLine($"\nHello {name}. \nNow we are going to do some interesting quizzery so you had better be ready.\n\n"); 
+            var questions = File.ReadAllLines("questions.csv");  //Read in questions.csv
+            foreach (var line in questions.Skip(1)) //Step through all questions in questions.csv skipping the first line as it is purely descriptive.
+            {
+                var question = line.Split(";"); //Grab a line from questions.csv and break the questions and answers into seperate pieces before writing them out
+                Console.WriteLine($"Okay {name}, {question[0]}?\n");
+                Console.WriteLine($"1. {question[1]}");
+                Console.WriteLine($"2. {question[2]}");
+                Console.WriteLine($"3. {question[3]}");
+                Console.WriteLine($"4. {question[4]}\n");
+                Console.WriteLine("Please press the number that corresponds to your answer.\n");
+                var ans = Console.ReadKey(true);
+                if (ans.KeyChar.ToString() == question[5]) //Comparing the quizzee's answer to whatever it was supposed to be acccording to questions.csv and giving them points or telling them they're trash
+                {
+                    score++;
+                    Console.WriteLine($"Good job {name}, you got it right. Your score is now {score}.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"{name}, {name}, {name}... Unbelievable. I can't believe you wouldn't know every bit of trivia out there!\n");
+                }
+
+            }
+            Console.WriteLine($"Good job {name}, you made it through all of the questions with a final score of {score}."); //Write final score to quizzee before quitting
+        }
+    } 
+}
