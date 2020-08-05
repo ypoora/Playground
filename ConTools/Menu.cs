6149fc26d3f2410ec7d224980e8953f75c81be46
Calvin Verheijen <ypoora@gmail.com>
2020-08-05 20:34:19 +0200

    Started "ConTools" classlib, refactoring
    Menu.cs now lives in a class library to make it universally usable. Useful!

--- ConTools/Menu.cs
+++ ConTools/Menu.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace PlayingWithLists
+namespace ConTools
 {
     public class Menu
     {
--- PlayingWithLists/Program.cs
+++ PlayingWithLists/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using ConTools;
 
 namespace PlayingWithLists
 {
@@ -8,12 +8,21 @@ namespace PlayingWithLists
     {
         public static List<Box> Stash { get; private set; }
         public static Container Container { get; set; }
-        
-        
+
 
         private static void Main(string[] args)
         {
-            var menuOptions = new List<(string, Action)> {("Look at your stash", Program.ShowStash), ("Add a new box", Program.FillBox), ("Empty your boxes", Program.EmptyBoxes), ("Load your container", () => Program.LoadBoxes(Program.Container)), ("Unload boxes from your container", Container.Unload), ("Show your container contents", Container.ListContents), ("Do the tutorial again", () => Program.Tutorial(Program.Container)), ("Quit", () => Environment.Exit(0)) };
+            var menuOptions = new List<(string, Action)>
+            {
+                ("Look at your stash", ShowStash), 
+                ("Add a new box", FillBox), 
+                ("Empty your boxes", EmptyBoxes),
+                ("Load your container", () => LoadBoxes(Program.Container)),
+                ("Unload boxes from your container", Container.Unload),
+                ("Show your container contents", Container.ListContents),
+                ("Do the tutorial again", () => Tutorial(Container)), 
+                ("Quit", () => Environment.Exit(0))
+            };
             Stash = new List<Box>(); //Make a list. Lists are good places to stash boxes. I swear.
             Container = new Container(); //Make a container, you know,for containing.
             Tutorial(Container);
@@ -26,7 +35,7 @@ namespace PlayingWithLists
         }
 
 
-        public static void Tutorial(Container container)
+        private static void Tutorial(Container container)
         {
             Console.WriteLine("Imagine this. You have boxes available in any colour you like.");
             Console.WriteLine("You have a bunch of items you could stash in there!");
@@ -37,8 +46,10 @@ namespace PlayingWithLists
                 FillBox();
                 i++;
             }
+
             ShowStash();
-            Console.WriteLine("\nYour boxes are full. We should make some room don't you think? Y/N\n"); //Empty the boxes if so desired. Rudely, that is.
+            Console.WriteLine(
+                "\nYour boxes are full. We should make some room don't you think? Y/N\n"); //Empty the boxes if so desired. Rudely, that is.
             var key = Console.ReadKey(true);
             if (key.KeyChar.ToString().ToLower() == "y")
             {
@@ -47,11 +58,14 @@ namespace PlayingWithLists
                 ShowStash();
                 Console.WriteLine("Now tell me something you really really like a lot.");
                 var NewThing = Console.ReadLine();
-                Console.WriteLine("\nCool. Did ya a favour; here's your new stash!\n"); //I'm going to put your favourite thing into all of your boxes. Aren't i nice?
+                Console.WriteLine(
+                    "\nCool. Did ya a favour; here's your new stash!\n"); //I'm going to put your favourite thing into all of your boxes. Aren't i nice?
                 FillAllBoxes(NewThing);
                 ShowStash();
             }
-            Console.WriteLine("\nYou also have a storage container. Guess we should put some boxes in there. Wanna do it? Y/N\n");
+
+            Console.WriteLine(
+                "\nYou also have a storage container. Guess we should put some boxes in there. Wanna do it? Y/N\n");
             key = Console.ReadKey(true);
             if (key.KeyChar.ToString().ToLower() == "y")
             {
@@ -62,25 +76,26 @@ namespace PlayingWithLists
                     Console.WriteLine($"{box.Colour} box with {box.Item}.");
                 }
             }
+
             ShowStash();
             Console.WriteLine("\nNow what? World's your oyster!\n");
         }
 
-        public static void LoadBoxes(Container container)
+        private static void LoadBoxes(Container container)
         {
             foreach (var box in Stash)
             {
                 container.Load(box);
                 Console.WriteLine($"The {box.Colour} box with {box.Item} was loaded.");
             }
+
             foreach (var box in container.ToRemoveFromStash)
             {
                 Stash.Remove(box);
             }
-
         }
 
-        public static void FillAllBoxes(string NewThing)
+        private static void FillAllBoxes(string NewThing)
         {
             foreach (var box in Stash)
             {
@@ -88,7 +103,7 @@ namespace PlayingWithLists
             }
         }
 
-        public static void EmptyBoxes()
+        private static void EmptyBoxes()
         {
             foreach (var box in Stash)
             {
@@ -96,29 +111,30 @@ namespace PlayingWithLists
             }
         }
 
-        public static void ShowStash()
+        private static void ShowStash()
         {
-            Console.WriteLine("\nYour stash contains the following:\n"); //Show off your stash of boxes and their contents. You're so organized.
+            Console.WriteLine(
+                "\nYour stash contains the following:\n"); //Show off your stash of boxes and their contents. You're so organized.
             foreach (var box in Stash)
             {
                 box.Describe();
             }
+
             Console.WriteLine();
         }
 
-        public static void FillBox()
+        private static void FillBox()
         {
-                Console.WriteLine("\nWhat's the colour of your box?");
-                var colour = Console.ReadLine();
+            Console.WriteLine("\nWhat's the colour of your box?");
+            var colour = Console.ReadLine();
 
-                Console.WriteLine("\nWhat's inside?");
-                var contents = Console.ReadLine();
+            Console.WriteLine("\nWhat's inside?");
+            var contents = Console.ReadLine();
 
-                var box = new Box(contents, colour);
-                Stash.Add(box);
+            var box = new Box(contents, colour);
+            Stash.Add(box);
 
-                Console.WriteLine($"\nCool, now you have a {box.Colour} box containing {box.Item}.\n");
+            Console.WriteLine($"\nCool, now you have a {box.Colour} box containing {box.Item}.\n");
         }
     }
-
 }
