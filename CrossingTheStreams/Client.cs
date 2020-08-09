950671d282a1df09595d40320296785979762eef
Calvin Verheijen <ypoora@gmail.com>
2020-08-09 14:37:17 +0200

    Tweaks to UI. Last build using old protocol.

---
+++ CrossingTheStreams/Client.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrossingTheStreams
+{
+    class Client
+    {
+        public static void NesdClient()
+        {
+            FileStream file;
+            long filesize;
+            Console.WriteLine("What file do you want to send?");
+            while (true)
+            {
+                var path = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    file = File.OpenRead(path);
+                    filesize = new FileInfo(path).Length;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Learn to type, dummy");
+                }
+            }
+
+            Console.WriteLine("Where's it gotta go? Please specify an IP address.");
+            while (true)
+            {
+                var address = Console.ReadLine();
+                IPAddress ipaddress;
+                if (!string.IsNullOrEmpty(address) && IPAddress.TryParse(address, out ipaddress))
+                {
+
+                    try
+                    {
+                        var client = new TcpClient();
+                        Console.WriteLine("Connecting...");
+                        client.Connect(ipaddress, 8000);
+                        var netstream = client.GetStream();
+                        Task task = file.CopyToAsync(netstream);
+                        Console.Clear();
+                        Console.CursorVisible = false;
+                        Console.WriteLine("Transferring... ");
+                        var origwidth = Console.WindowWidth;
+                        do
+                        {
+                            Thread.Sleep(500);
+                            if (Console.WindowWidth < origwidth)
+                            {
+                                Console.SetWindowSize(origwidth, Console.WindowHeight);
+                            }
+
+                            var percent = Math.Round(file.Position / (double) filesize * 100);
+                            Console.SetCursorPosition(16, 0);
+                            Console.Write((percent + "%").PadLeft(4));
+                            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+                            while (Console.CursorLeft <= Console.WindowWidth - 2)
+                            {
+                                double barposition = Console.WindowWidth * percent / 100;
+                                if (Console.CursorLeft <= barposition)
+                                {
+                                    Console.Write("=");
+                                }
+                                else
+                                {
+                                    Console.Write(" ");
+                                }
+                            }
+                        } while (!task.IsCompleted);
+                        Console.CursorVisible = true;
+                        client.Close();
+                        Console.Clear();
+                        if (task.IsCompletedSuccessfully)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("File sent OK!\n");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            break;
+                        }
+                        else
+                        {
+                            client.Dispose();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(
+                                "Something went wrong. Check the network and receiver, then try again.\n");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Something went wrong. Check the file, network and receiver, then try again.\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+                    catch (SocketException)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("A network error occured. Check the IP address and connection, then try again.\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Learn to type, dummy");
+                }
+            }
+
+            Program.Main();
+        }
+    }
+}
---
+++ CrossingTheStreams/Server.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CrossingTheStreams
+{
+    class Server
+    {
+        public static void NesdServer()
+        {
+            FileStream file;
+            var listener = new TcpListener(IPAddress.Any,8000);
+            listener.Start();
+            Console.WriteLine("Server started.");
+            Console.Clear();
+            while (true)
+            {
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(2,1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write ("Ready, awaiting connection.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Thread.Sleep(250);
+                var path = "";
+                if (listener.Pending())
+                {
+                    try
+                    {
+                        var client = listener.AcceptTcpClient();
+                        Console.SetCursorPosition(2,1);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Client connecting... Enter path to save file: ");
+                        Console.CursorVisible = true;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        while (true)
+                        {
+                            path = Console.ReadLine();
+                            if (!string.IsNullOrEmpty(path))
+                            {
+                                Console.CursorVisible = false;
+                                Console.SetCursorPosition(2,2);
+                                Console.WriteLine("Receiving file...");
+                                file = File.OpenWrite(path);
+                                var netstream = client.GetStream();
+                                netstream.CopyTo(file);
+                                file.Dispose();
+                                Console.SetCursorPosition(2,2);
+                                Console.WriteLine($"File {path} was stored.");
+                                Console.SetCursorPosition(2,1);
+                                while (Console.CursorLeft <= Console.WindowWidth - 2)
+                                {
+                                    Console.Write(" ");
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                Console.SetCursorPosition(0,0);
+                                Console.WriteLine("Type something,dummy");
+                            }
+                        }
+                    }
+                    catch (IOException)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Something went wrong. Check the network and sender, then try again.\n");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("Deleting the failed partial file... ");
+                        try
+                        {
+                            File.Delete(path);
+                            Console.WriteLine("File deleted.");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Deletion failed. File may not have been created yet.");
+                        }
+
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
+}
