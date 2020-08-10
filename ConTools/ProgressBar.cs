5a7a5a98f6415e5a3ba6f4f5caf5f50ecd66233b
Calvin Verheijen <ypoora@gmail.com>
2020-08-10 22:18:52 +0200

    Major changes to server, concurrent transfers
    Implemented threading to allow for concurrent transfers to be handles by the Server
    
    Made a console UI for a quick status overview, runs in own thread.
    
    Tweaks to ProgressBar.cs adding a small inmine bar and tweaking the main one.

--- ConTools/ProgressBar.cs
+++ ConTools/ProgressBar.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace ConTools
@@ -27,7 +28,7 @@ namespace ConTools
             try
             {
                 if(showPercent) Console.SetCursorPosition(Console.WindowWidth / 2 - 2, Console.WindowHeight - 1);
-                if(showPercent) Console.Write($"{percent}%");
+                if(showPercent) Console.Write($"{Math.Floor(percent)}%");
                 Console.SetCursorPosition(0, Console.WindowHeight - 2);
                 var barLength = Math.Floor(Console.WindowWidth * (percent / 100));
                 var builder = new StringBuilder();
@@ -42,6 +43,24 @@ namespace ConTools
             {
                 Console.Clear();
             }
+        }
+
+        /// <summary>
+        /// Prints an in-line progress bar with configurable length and percentage.
+        /// </summary>
+        /// <param name="percent">Percentage to draw (0-100, accepts doubles)</param>
+        /// <param name="length">Length of the bar.</param>
+        /// <param name="showPercent">Display percentage true/false</param>
+        public static void InlineProgress(double percent, int length, bool showPercent)
+        {
+            var barLength = Math.Floor(length * (percent / 100));
+            var builder = new StringBuilder();
+            for (var i = 0; i < barLength; i++)
+            {
+                builder.Append("=");
+            }
+            Console.Write("[" + builder.ToString().PadRight(length) + "]");
+            if (showPercent) Console.Write($" {(Math.Floor(percent)+ "%").PadRight(4)}");
         }
     }
 }
--- CrossingTheStreams/Server.cs
+++ CrossingTheStreams/Server.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -10,127 +11,167 @@ namespace CrossingTheStreams
 {
     class Server
     {
+        private static ObservableCollection<(string, FileStream, long)> _receiving;
+        private static (int, int) _windowSize = (0, 0);
         public static void NesdServer()
         {
-            FileStream file;
+            _receiving = new ObservableCollection<(string, FileStream, long)>();
             var listener = new TcpListener(IPAddress.Any,8000);
             listener.Start();
             Console.WriteLine("Server started.");
             Console.Clear();
+            var uiThread = new Thread(ConsoleUI);
+            uiThread.Start();
             while (true)
             {
-                Console.CursorVisible = false;
-                Console.SetCursorPosition(2,1);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write ("Ready, awaiting connection.");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Thread.Sleep(250);
-                var path = "";
-                if (listener.Pending())
+                //If a connection is pending, start a new thread which then accepts it and receives the file
+                if (listener.Pending()) 
                 {
-                    try
-                    {
-                        var client = listener.AcceptTcpClient();
-                        //Console.SetCursorPosition(2,1);
-                        //Console.ForegroundColor = ConsoleColor.Yellow;
-                        //Console.Write("Client connecting... Enter path to save file: ");
-                        //Console.CursorVisible = true;
-                        //Console.ForegroundColor = ConsoleColor.Gray;
-                        while (true)
-                        {
-                            //TODO: turn these into extension methods for code cleanness
-                            //Read in the 
-                            var netstream = client.GetStream();
-                            var nameLenBuf = new byte[4];
-                            netstream.Read(nameLenBuf, 0, 4);
-                            var nameLen = BitConverter.ToInt32(nameLenBuf);
-                            var nameBuf = new byte[nameLen];
-                            netstream.Read(nameBuf, 0, nameLen);
-                            var name = Encoding.UTF8.GetString(nameBuf);
-                            var lenBuf = new byte[8];
-                            netstream.Read(lenBuf, 0, 8);
-                            var len = BitConverter.ToInt64(lenBuf);
-
-
-                            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), name);
-                            //path = Console.ReadLine();
-                            if (!string.IsNullOrEmpty(path))
-                            {
-                                //Console.CursorVisible = false;
-                                Console.SetCursorPosition(2,2);
-                                Console.WriteLine("Receiving file...");
-                                file = File.OpenWrite(path + name);
-                                var task = netstream.CopyToAsync(file);
-                                Console.Clear();
-                                //Console.CursorVisible = false;
-                                do
-                                {
-                                    ProgressBar.ShowProgress(Math.Ceiling((double) file.Position / len * 100), $"Receiving {name}...", true);
-                                    Thread.Sleep(500);
-                                } while (!task.IsCompleted);
-
-                                if (task.IsCompletedSuccessfully)
-                                {
-                                    Console.Clear();
-                                    file.Dispose();
-                                    Console.SetCursorPosition(2, 2);
-                                    Console.WriteLine($"File {name} was stored.");
-                                }
-                                else
-                                {
-                                    file.Dispose();
-                                    Console.ForegroundColor = ConsoleColor.Red;
-                                    Console.WriteLine("Something went wrong. Check the network and sender, then try again.\n");
-                                    Console.ForegroundColor = ConsoleColor.Gray;
-                                    Console.WriteLine("Deleting the failed partial file... ");
-                                    try
-                                    {
-                                        File.Delete(path);
-                                        Console.WriteLine("File deleted.");
-                                    }
-                                    catch
-                                    {
-                                        Console.WriteLine("Deletion failed. File may not have been created yet.");
-                                    }
-
-                                    Thread.Sleep(1000);
-                                    Console.Clear();
-                                }
-
-                                Console.SetCursorPosition(2,1);
-                                while (Console.CursorLeft <= Console.WindowWidth - 2)
-                                {
-                                    Console.Write(" ");
-                                }
-                                break;
-                            }
-                            else
-                            {
-                                Console.SetCursorPosition(0,0);
-                                Console.WriteLine("Type something,dummy");
-                            }
-                        }
-                    }
-                    catch (IOException)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Something went wrong. Check the network and sender, then try again.\n");
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.WriteLine("Deleting the failed partial file... ");
-                        try
-                        {
-                            File.Delete(path);
-                            Console.WriteLine("File deleted.");
-                        }
-                        catch
-                        {
-                            Console.WriteLine("Deletion failed. File may not have been created yet.");
-                        }
-
-                        Thread.Sleep(1000);
-                        Console.Clear();
-                    }
+                    var receive = new Thread(() => ReceiveFile(listener));
+                    receive.Start();
                 }
+            }
+        }
+
+        private static void ConsoleUI()
+        {
+            var listChanged = false;
+            while (true)
+            {
+                //Probe if the amount of transferring items has changed, refresh console if so
+                _receiving.CollectionChanged += (sender, args) => listChanged = true;
+                if(listChanged) Console.Clear();
+                listChanged = false;
+                
+                //probe if the size of the console window has changed, refresh console if so
+                if (_windowSize != (Console.WindowWidth, Console.WindowHeight))
+                {
+                    _windowSize = (Console.WindowWidth, Console.WindowHeight);
+                    Console.Clear();
+                }
+                
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(2, 1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                
+                //Bit of a blanket statement, but if we're NOT ready i'm sure there will be other fun stuff on the screen
+                Console.Write("Ready, accepting connections.\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                
+                //Display receiving files from collection, with progress for each
+                foreach (var item in _receiving)
+                {
+                    Console.Write("  ");
+                    ProgressBar.InlineProgress((double) item.Item2.Position / item.Item3 * 100, Console.WindowWidth / 3, true);
+                    Console.Write("receiving " + item.Item1);
+                    Console.WriteLine();
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void ReceiveFile(TcpListener listener)
+        {
+            FileStream file;
+            var path = "";
+            try
+            {
+                var client = listener.AcceptTcpClient();
+                //Console.SetCursorPosition(2,1);
+                //Console.ForegroundColor = ConsoleColor.Yellow;
+                //Console.Write("Client connecting... Enter path to save file: ");
+                //Console.CursorVisible = true;
+                //Console.ForegroundColor = ConsoleColor.Gray;
+                while (true)
+                {
+                    //Read in the file attributes
+                    var netstream = client.GetStream();
+                    var nameLen = BitConverter.ToInt32(netstream.ReadExactly(4));
+                    var name = Encoding.UTF8.GetString(netstream.ReadExactly(nameLen));
+                    var len = BitConverter.ToInt64(netstream.ReadExactly(8));
+                    
+                    //generate destination path based on current user's desktop TODO:make this configurable to any location
+                    path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), name);
+
+                    //Sanity-check path, add file to list of transfers and copy it.
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        file = File.OpenWrite(path);
+                        var listitem = (name, file, len);
+                        _receiving.Add(listitem);
+                        var task = netstream.CopyToAsync(file);
+                        task.Wait();
+                        _receiving.Remove(listitem);
+
+
+                        //Console.CursorVisible = false;
+                        // do
+                        // {
+                        // ProgressBar.ShowProgress(Math.Ceiling((double) file.Position / len * 100), $"Receiving {name}...", true);
+                        // Thread.Sleep(500);
+                        // } while (!task.IsCompleted);
+                        
+                        if (task.IsCompletedSuccessfully)
+                        {
+                            Console.Clear();
+                            file.Dispose();
+                            Console.SetCursorPosition(2, Console.WindowHeight);
+                            Console.WriteLine($"File {name} was stored.");
+                        }
+                        else
+                        {
+                            file.Dispose();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Something went wrong. Check the network and sender, then try again.\n");
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine("Deleting the failed partial file... ");
+                            try
+                            {
+                                File.Delete(path);
+                                Console.WriteLine("File deleted.");
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Deletion failed. File may not have been created yet.");
+                            }
+                            
+
+                            Thread.Sleep(1000);
+                            Console.Clear();
+                        }
+
+                        Console.SetCursorPosition(2, 1);
+                        while (Console.CursorLeft <= Console.WindowWidth - 2)
+                        {
+                            Console.Write(" ");
+                        }
+
+                        break;
+                    }
+
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine("Type something,dummy");
+                }
+            }
+            catch (IOException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Something went wrong. Check the network and sender, then try again.\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("Deleting the failed partial file... ");
+                try
+                {
+                    File.Delete(path);
+                    Console.WriteLine("File deleted.");
+                }
+                catch
+                {
+                    Console.WriteLine("Deletion failed. File may not have been created yet.");
+                }
+
+                Thread.Sleep(1000);
+                Console.Clear();
             }
         }
     }
